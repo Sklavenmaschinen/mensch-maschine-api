9fcdd906b48b2c933693d6ebc9e1a02d761260f4
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2018-07-16 22:34:31 -0300

    First look and feel + Jobs list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, Redirect, Link, withRouter } from 'react-router-dom';
+
+import Overview from './components/Overview';
+import Workers from './components/Workers';
+import Jobs from './components/Jobs';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    const Nav = withRouter(props => (
+      <ul className="nav-bar">
+        <li className={props.location.pathname === "/" ? "active" : ""}><Link to="/">Overview</Link></li>
+        <li className={props.location.pathname === "/workers" ? "active" : ""}><Link to="/workers">Workers</Link></li>
+        <li className={props.location.pathname === "/jobs" ? "active" : ""}><Link to="/jobs">Jobs</Link></li>
+      </ul>
+    ));
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <header>
+            <h4><small>Die</small>Mensch Maschine</h4>
+          </header>
+          <hr />
+          <Nav />
+          <div className="page-content">
+            <Switch>
+              <Route path="/" exact={true} component={Overview} />
+              <Route path="/workers" component={Workers} />
+              <Route path="/jobs" component={Jobs} />
+              <Route render={() => <Redirect to="/" />} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
