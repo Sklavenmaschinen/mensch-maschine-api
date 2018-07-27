d0d8f6d0dec705eb6bf44d72f2a085bc1010efee
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2018-07-27 14:42:28 -0300

    Initial commit

--- src/test/java/com/menschmaschine/api/ApplicationTests.java
+++ src/test/java/com/menschmaschine/api/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.menschmaschine;
+package com.menschmaschine.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
